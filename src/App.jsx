5f1c35e1891f67a903751284ef0e2e2ec994b47c
Heechem <hichempeter@gmail.com>
2023-09-06 20:39:49 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="mb-10 text-3xl font-bold underline">tailwind</h1>
+      <h1 className="text-xl underline">Hello</h1>
+    </>
+  );
+}
+
+export default App;
